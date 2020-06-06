2c1ff6df8ef8c5dac1f903099f0486709742411f
Rafal Mi <rafal.m4@gmail.com>
2020-06-06 14:49:27 +0200

    Connected JSXGraph. Added Debug configuration

---
+++ src/components/JSXGraph.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="jsxgraph-box" class="jxgbox" style="width:400px; height:400px;">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MathJax',
+  mounted() {
+    var board = window.JXG.JSXGraph.initBoard('jsxgraph-box', {boundingbox: [-2, 4, 6, -4], axis:true, grid:true});
+  },
+}
+</script>
