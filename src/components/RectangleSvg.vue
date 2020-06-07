42d9bf87f301bbabdbaa9b4704593495c4700b45
Rafal Mi <rafal.m4@gmail.com>
2020-06-07 18:50:48 +0200

    Components Rectangle SVG and Arrow

---
+++ src/components/RectangleSvg.vue
@@ -0,0 +1,94 @@
+<template>
+  <svg width="130" height="60">
+    <defs>
+      <linearGradient
+        id="grad1"
+        :x1="gradient.x1"
+        :y1="gradient.y1"
+        :x2="gradient.x2"
+        :y2="gradient.y2"
+      >
+        <stop offset="0%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+        <stop offset="100%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+      </linearGradient>
+    </defs>
+    <rect x="20" y="0" width="90" height="60" rx="10" fill="url(#grad1)" />
+    <text x="10" y="8" font-size="16">{{ names[0] }}</text>
+    <text x="120" y="8" font-size="16">{{ names[1] }}</text>
+    <text x="120" y="52" font-size="16">{{ names[2] }}</text>
+    <text x="10" y="52" font-size="16">{{ names[3] }}</text>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "RectangleSVG",
+  props: ["permutation"],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    names() {
+      switch (this.permutation) {
+        case "identity":
+          return ['A', 'B', 'C', 'D'];
+        case "rotation": // Rotation
+          return ['C', 'D', 'A', 'B'];
+        case "reflection-vertical": // Reflection vertical
+          return ['B', 'A', 'D', 'C'];
+        case "reflection-horizontal": // Reflection horizontal
+          return ['D', 'C', 'B', 'A'];
+      }
+      return ['X', 'X', 'X', 'X'];
+    },
+    gradient() {
+      let g = {};
+      switch (this.permutation) {
+        case "identity":
+          g.x1 = "0%";
+          g.y1 = "0%";
+          g.x2 = "100%";
+          g.y2 = "100%";
+          return g;
+        case "rotation": // Rotation
+          g.x1 = "100%";
+          g.y1 = "100%";
+          g.x2 = "0%";
+          g.y2 = "0%";
+          return g;
+        case "reflection-vertical": // Reflection vertical
+          g.x1 = "100%";
+          g.y1 = "0%";
+          g.x2 = "0%";
+          g.y2 = "100%";
+          return g;
+        case "reflection-horizontal": // Reflection horizontal
+          g.x1 = "0%";
+          g.y1 = "100%";
+          g.x2 = "100%";
+          g.y2 = "0%";
+          return g;
+      }
+      return g;
+    }
+  },
+};
+</script>
+
+<style scoped>
+svg {
+  /* border:1px solid red; */
+}
+
+text {
+  text-anchor: middle;
+  dominant-baseline: middle;
+  fill: rgb(227, 66, 52);
+}
+
+body {
+  font-family: sans-serif;
+}
+</style>
---
+++ src/views/ch03groups/Exe03.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="exe-03 mx-4 my-4" :key="latex">
+    <span class="title">Exercise 3.</span>
+    <p>
+      Write out Cayley tables for groups formed by the symmetries of a rectangle and for 
+      \( (\mathbb{Z}_4, +) \)
+      How many elements are in each group? Are the groups the same? Why or why not?
+    </p>
+    <p>
+      <span class="title">Solution</span>
+    </p>
+    <p>
+      There are four symmetries of rectangle.
+    </p>
+    <p>
+      Identity
+    </p>
+    <p>
+      <rectangle-svg permutation="identity"></rectangle-svg>
+      <arrow>\( 1 \)</arrow>
+      <rectangle-svg permutation="identity"></rectangle-svg>
+    </p>
+    <p>
+      Rotation \( 180^\circ \)
+    </p>
+    <p>
+      <rectangle-svg permutation="identity"></rectangle-svg>
+      <arrow>\( \rho \)</arrow>
+      <rectangle-svg permutation="rotation"></rectangle-svg>
+    </p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import RectangleSvg from '@/components/RectangleSvg';
+import Arrow from '@/components/Arrow';
+
+export default {
+  name: 'Exe03',
+  components: {
+    RectangleSvg, Arrow
+  },
+  data() {
+    return {
+      latex: null
+    }
+  },
+  watch: {
+    latex: function() {
+      console.log("Key changed");
+      this.$nextTick().then(() => {
+        this.reRender();
+      });
+    }
+  }
+
+}
+</script>
