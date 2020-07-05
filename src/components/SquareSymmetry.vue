14f1055fc8324177f50835970f4304b4281b0331
Rafal Mi <rafal.m4@gmail.com>
2020-07-05 20:36:19 +0200

    More on Exercise 3.4.5

---
+++ src/components/SquareSymmetry.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>
+      <slot></slot>
+    </p>
+    <div class="element-row">
+      <square-svg permutation="identity" gradId="1"></square-svg>
+      <arrow><slot name="name"></slot></arrow>
+      <square-svg :permutation="permutation" :gradId="gradId"></square-svg>
+      <div>
+        <slot name="permutation"></slot>
+      </div>
+    </div>
+  </div> 
+</template>
+
+<script>
+import SquareSvg from '@/components/SquareSvg';
+import Arrow from '@/components/Arrow';
+
+export default {
+  name: "square-symmetry",
+  props: ["permutation", "gradId"],
+  components: {
+    SquareSvg, Arrow
+  }
+}
+</script>
+
+<style scoped>
+.element-row {
+  display: flex;
+  align-items: center;
+}
+</style>
